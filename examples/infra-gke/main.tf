f6cbeb48795b6b37c767b90f954fd18570044336
Azri Jamil <azri@nematix.com>
2023-04-08 22:58:25 +0800

    feat: infra gke example

--- examples/infra-gke/main.tf
+++ examples/infra-gke/main.tf
@@ -1,15 +1,15 @@
 module "google-infra-gke" {
-  source         = "../../"
+  source = "../../"
 
-  app_org_id     = var.app_org_id
-  project_id     = var.project_id
-  region         = var.region
-  zone_default   = var.zone_default
-  env            = var.env
-  app_domain     = var.app_domain
-  client_id      = var.client_id
-  client_secret  = var.client_secret
-  enable_service_gke = true
+  app_org_id                  = var.app_org_id
+  project_id                  = var.project_id
+  region                      = var.region
+  zone_default                = var.zone_default
+  env                         = var.env
+  app_domain                  = var.app_domain
+  client_id                   = var.client_id
+  client_secret               = var.client_secret
+  enable_service_gke          = true
   enable_service_gke_operator = true
-  enable_service_firebase = false
+  enable_service_firebase     = false
 }
